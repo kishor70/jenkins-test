8b19d70dd44f443e3292f1ffde35a6ab711a082a
kishor <kishorgbss3@gmail.com>
2025-05-27 10:33:24 +0530

    Adding more line

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Hello from jenkins");
         System.out.println("This is for learning jenkins");
+        System.out.println("Addin new line");
     }
     
 }
