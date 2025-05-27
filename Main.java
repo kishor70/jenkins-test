d0924eaa426283f1ead1cd24ef246807460da0e2
kishor <kishorgbss3@gmail.com>
2025-05-27 09:38:21 +0530

    Changes in initial commit

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package Jenkins;
 class Main{
     public static void main(String[] args) {
         System.out.println("Hello from jenkins");
+        System.out.println("This is for learning jenkins");
     }
     
 }
